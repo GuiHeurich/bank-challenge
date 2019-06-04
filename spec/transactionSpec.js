ec15315a67d963aff7757d543ad27a865c72d879
guilhe0756 <guiheurich@gmail.com>
2019-06-04 12:38:06 +0100

    Extract the Date object
    A new transaction now generates its own date instead of having one passed in in the Account function. This extracts behaviour from the Account which shouldn't belong to it, sucessfully passing it to the Transaction account.

--- spec/transactionSpec.js
+++ spec/transactionSpec.js
@@ -5,28 +5,37 @@ describe('Transaction', function(){
   var baseTime = new Date(2019, 5, 3)
 
   it('returns the values of a deposit', function(){
-    transaction = new Transaction(baseTime, "credit", 1000, 1000)
-    expect(transaction.getDate()).toBe("03/06/19")
+    transaction = new Transaction("credit", 1000, 1000)
     expect(transaction.getName()).toBe("credit");
     expect(transaction.getValue()).toBe(1000);
     expect(transaction.getBalance()).toBe(1000)
   })
 
-  it('returns the date of a deposit', function(){
-    transaction = new Transaction(baseTime, "credit", 1000, 1000)
-    expect(transaction.getDate()).toBe("03/06/19")
-  })
-
   it('returns the values of a withdrawal', function(){
-    transaction = new Transaction(baseTime, "debit", 400, 600)
-    expect(transaction.getDate()).toBe("03/06/19")
+    transaction = new Transaction("debit", 400, 600)
     expect(transaction.getName()).toBe("debit");
     expect(transaction.getValue()).toBe(400);
     expect(transaction.getBalance()).toBe(600)
   })
 
-  it('returns the date of a withdrawal', function(){
-    transaction = new Transaction(baseTime, "debit", 400, 600)
-    expect(transaction.getDate()).toBe("03/06/19")
+  describe('Transaction', function(){
+    var transaction;
+
+    beforeEach(function(){
+      transaction = {
+        getDate: function(date){
+          return date;
+        }
+      }
+
+      spyOn(transaction, 'getDate');
+
+      transaction.getDate("03/06/19");
+    })
+
+    it("tracks that the transaction returns a date", function(){
+      expect(transaction.getDate).toHaveBeenCalled();
+    })
+
   })
 });
